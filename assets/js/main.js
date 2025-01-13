3bfa32c8a34c76a93fab6d91492158ee15064223
Dheeraj Mishra <54015377+dheeraj992@users.noreply.github.com>
2025-01-14 01:43:37 +0530

    css and js changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 var itemsCount = 3;
-var finalPrice = 15097
+var intialPrice = 15097
+var finalPrice
+var cartItem = 1;
+
+const products = {
+
+    
+    "Suzuki Streeing" : "Suzuki Steering systems are known for their precision and reliability, offering superior handling and comfort for drivers. With a focus on innovation and advanced technology, Suzuki steering mechanisms are designed to provide a smooth and responsive driving experience. Whether it's power steering or electronic steering, Suzuki ensures that every vehicle is equipped with an easy-to-use, durable, and efficient steering system to enhance overall performance and safety on the road.",
+    "Infotainment System" : "Infotainment System in modern vehicles provides a seamless integration of entertainment, navigation, and connectivity features. It allows drivers and passengers to enjoy music, movies, apps, and GPS navigation with ease. With features like touchscreens, voice control, and smartphone connectivity, the infotainment system enhances the driving experience by keeping drivers informed, entertained, and connected, all while minimizing distractions on the road.",
+    "Suzuki Gear Knob" : "Suzuki Gear Knob is designed to provide a smooth and ergonomic shifting experience for drivers. Crafted with precision and durability, it offers a comfortable grip, allowing for easy and responsive gear changes. Whether it's a manual or automatic transmission, Suzuki’s gear knobs are designed to enhance the vehicle’s interior aesthetics while improving the overall driving experience with their functional and stylish design."
+}
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
@@ -70,14 +80,64 @@ const scrollActive = () =>{
 function handleDelete(icon){
 
     const container = icon.closest('.cart__card')
+    let price = container.querySelector('.cart__price')
+    price = price.innerHTML
+    price = Number.parseInt(price.replace("Rs.",""),10)
+    finalPrice = intialPrice - price
     container.style.display = 'none'
-    
+   
     if(itemsCount > 0){
 
         itemsCount -- ;
         UpdateCartItems()
     }
     
+}
+
+function handlePlus(icon){
+
+    let parentContainer = icon.closest('.cart__amount-content')
+    let itemCount = parentContainer.querySelector('.cart__amount-number')
+    let cartDetails = icon.closest('.cart__details')
+    let itemPrice = cartDetails.querySelector('.cart__price')
+    itemPrice = itemPrice.innerHTML
+    itemPrice = Number.parseInt(itemPrice.replace("Rs.",""),10)
+    cartItem = itemCount.innerHTML
+    cartItem ++
+    itemCount.innerHTML = cartItem
+
+    updateTotalPrice(itemPrice,cartItem)
+
+}
+
+function handleMinus(icon){
+
+    let parentContainer = icon.closest('.cart__amount-content')
+    let itemCount = parentContainer.querySelector('.cart__amount-number')
+    let newCount = itemCount.innerHTML
+    newCount--
+    itemCount.innerHTML = newCount
+
+}
+
+function handleDescription(icon){
+
+    let container = icon.closest('.featured__card')
+    let itemHeading = container.querySelector('.featured__title')
+    console.log('itemHeading-->'+itemHeading.innerHTML)
+    let modalOverlay = document.querySelector('.modal-overlay');
+    let modalHeader = document.querySelector('.modal-header')
+    let modalBody = document.querySelector('.modal-body')
+    modalHeader.innerHTML = itemHeading.innerHTML
+    modalBody.innerHTML = products[itemHeading.innerHTML]
+    modalOverlay.style.display = 'block';
+    
+}
+
+function handleClose(){
+    let modalOverlay = document.querySelector('.modal-overlay');
+    let closeModalBtn = document.querySelector('.close-btn');
+    modalOverlay.style.display = 'none'
 
 }
 
@@ -96,6 +156,18 @@ else{
     totalPrice.style.display = 'none'
     
 }
+}
+
+function updateTotalPrice(itemPrice,itemCount){
+
+let totalItem = itemCount - 1; // as intially one item is already added to the total price 
+let priceOfItem = itemPrice
+
+let totalPrice = intialPrice + (totalItem*priceOfItem)
+
+    const totalPriceSelector = document.querySelector('.cart__prices-total')
+    totalPriceSelector.innerHTML = ` Rs. ${totalPrice}`
+
 }
 window.addEventListener('scroll', scrollActive)
 const scrollUp = () =>{
